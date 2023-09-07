39f0a0e4beae39920c0a5d9e53491341bae64b97
Haniel Ulises <hanielulises2003@gmail.com>
2023-09-06 20:07:39 -0600

    Reader and writer function

--- image-writer/imageWriter.c
+++ image-writer/imageWriter.c
@@ -1,36 +1,47 @@
 #include "imageWriter.h"
 
-
+// Function to read an image file
 void imageReader(const char* imageName, int *_height, int *_width, int *_bitDepth ,unsigned char *_header, unsigned char *_colorTable, unsigned char *_buffer){
     FILE *streamIn;
-    streamIn =fopen(imageName, "rb");
+    streamIn = fopen(imageName, "rb");
 
     if(streamIn == (FILE *) 0){
         printf("Unable to read the image\n");
     }
 
+    // Read the header of the image
     for(int i = 0; i < 54; i++){
         _header[i] = getc(streamIn);
     }
 
+    // Extract image width, height, and bit depth from the header
     *_width  = * (int*)&_header[18];
     *_height= * (int*)&_header[22];
     *_bitDepth = *(int*)&_header[28];
 
+    // If the image has a bit depth less than or equal to 8, read the color table
     if(*_bitDepth <= 8){
-        fread(_colorTable, sizeof(unsigned char), 1024, streamIn);;
+        fread(_colorTable, sizeof(unsigned char), 1024, streamIn);
     }
 
+    // Read the image data into the buffer
     fread(_buffer, sizeof(unsigned), CUSTOM_IMG_SIZE, streamIn);
     fclose(streamIn);
 }
 
+// Function to write an image file
 void imageWriter(const char* imageName, unsigned char *header, unsigned char *colorTable, unsigned char* buffer, int bitDepth){
     FILE *fo = fopen(imageName, "wb");
-    fwrite(header, sizeof(unsigned char),54, fo);
+
+    // Write the header of the image 
+    fwrite(header, sizeof(unsigned char), 54, fo);
+
+    // If the image has a bit depth less than or equal to 8, write the color table
     if(bitDepth <= 8){
         fwrite(colorTable, sizeof(unsigned char), 1024, fo);
     }
+
+    // Write the image data from the buffer
     fwrite(buffer, sizeof(unsigned char), CUSTOM_IMG_SIZE, fo);
     fclose(fo);
 }
--- image-writer/main.c
+++ image-writer/main.c
@@ -1,17 +1,17 @@
 #include "imageWriter.h"
 
-int main(int argc, char *argv){
+int main(int argc, char *argv[]) {
     int imgWidth, imgHeight, imgBitDepth;
     unsigned char imgHeader[BMP_HEADER_SIZE];
     unsigned char imgColorTable[BMP_COLOR_TABLE_SIZE];
     unsigned char imgBuffer[CUSTOM_IMG_SIZE];
 
     const char imageName[] = "images/bridge.bmp";
-    const char newImgName = "images/bride_copy.bmp";
+    const char newImgName[] = "images/bride_copy.bmp";
 
     imageReader(imageName, &imgWidth, &imgHeight, &imgBitDepth, &imgHeader[0], &imgColorTable[0], &imgBuffer[0]);
-    imageWriter(newImgName, imgHeader, imgColorTable,imgBuffer, imgBitDepth);
+    imageWriter(newImgName, imgHeader, imgColorTable, imgBuffer, imgBitDepth);
 
-    printf("==== SUCESS ====\n");
-    return 0; 
+    printf("==== SUCCESS ====\n");
+    return 0;
 }
