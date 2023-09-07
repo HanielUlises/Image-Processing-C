efbfa4b8db61e32f96321168aacebe8a7d8cd08a
Haniel Ulises <hanielulises2003@gmail.com>
2023-09-06 20:02:40 -0600

    Reader and writer function

--- image-writer/imageWriter.c
+++ image-writer/imageWriter.c
@@ -1,8 +1,36 @@
 #include "imageWriter.h"
 
-#define BMP_HEADER_SIZE 54
-#define BMP_COLOR_TABLE_SIZE 1024
 
-void imageWriter(){
-    
+void imageReader(const char* imageName, int *_height, int *_width, int *_bitDepth ,unsigned char *_header, unsigned char *_colorTable, unsigned char *_buffer){
+    FILE *streamIn;
+    streamIn =fopen(imageName, "rb");
+
+    if(streamIn == (FILE *) 0){
+        printf("Unable to read the image\n");
+    }
+
+    for(int i = 0; i < 54; i++){
+        _header[i] = getc(streamIn);
+    }
+
+    *_width  = * (int*)&_header[18];
+    *_height= * (int*)&_header[22];
+    *_bitDepth = *(int*)&_header[28];
+
+    if(*_bitDepth <= 8){
+        fread(_colorTable, sizeof(unsigned char), 1024, streamIn);;
+    }
+
+    fread(_buffer, sizeof(unsigned), CUSTOM_IMG_SIZE, streamIn);
+    fclose(streamIn);
+}
+
+void imageWriter(const char* imageName, unsigned char *header, unsigned char *colorTable, unsigned char* buffer, int bitDepth){
+    FILE *fo = fopen(imageName, "wb");
+    fwrite(header, sizeof(unsigned char),54, fo);
+    if(bitDepth <= 8){
+        fwrite(colorTable, sizeof(unsigned char), 1024, fo);
+    }
+    fwrite(buffer, sizeof(unsigned char), CUSTOM_IMG_SIZE, fo);
+    fclose(fo);
 }
--- image-writer/imageWriter.h
+++ image-writer/imageWriter.h
@@ -1,5 +1,9 @@
+#include "stdio.h"
 #include "stdlib.h"
 
-void imageWriter(){
-    
-}
+#define BMP_HEADER_SIZE 54
+#define BMP_COLOR_TABLE_SIZE 1024
+#define CUSTOM_IMG_SIZE 1024*1024
+
+void imageReader(const char* imageName, int *_height, int *_width, int *_bitDepth ,unsigned char *_header, unsigned char *_colorTable, unsigned char *_buffer);
+void imageWriter(const char* imageName, unsigned char *header, unsigned char *colorTable, unsigned char* buffer, int bitDepth);
