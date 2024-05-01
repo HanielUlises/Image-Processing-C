c307687aea987818d6923dd15b540165012652b4
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-04-30 22:35:02 -0600

    Correct image brightness

---
+++ image-histogram/Brightness-Correction/brightness_correction.h
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#define BMP_HEADER_SIZE 54
+#define BMP_COLOR_TABLE_SIZE 1024
+#define CUSTOM_IMG_SIZE 1024*1024
+
+void imageReader(const char* imageName, int *_height, int *_width, int *_bitDepth ,unsigned char *_header, unsigned char *_colorTable, unsigned char *_buffer);
+void imageWriter(const char* imageName, unsigned char *header, unsigned char *colorTable, unsigned char* buffer, int bitDepth);
+void correctBrightness (unsigned char *_input_image_data, unsigned char *_output_image_data, int img_rows, int img_cols, int brightness);
