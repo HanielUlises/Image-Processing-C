c84da6f3e24af8e67bab328fa8774546a1506a5e
HanielUlises <hanielulises2003@gmail.com>
2026-07-25 17:25:57 -0600

    Fixed grayscale display

--- src/imageGrayscale.c
+++ src/imageGrayscale.c
@@ -19,15 +19,27 @@ void toGrayScale(const char* inputPath, const char* outputPath){
     imageReader(inputPath, &height, &width, &bitDepth,
                 header, colorTable, buffer);
 
-    int imageSize = width * height * (bitDepth / 8);
+    int channels  = bitDepth / 8;
+    int imageSize = width * height * channels;
 
-    for(int i = 0; i < imageSize; i++){
-        buffer[i] = (unsigned char)(
-            0.3 * buffer[i] +
-            0.59 * buffer[i] +
-            0.11 * buffer[i]
-        );
+    if (channels >= 3) {
+        /* Buffer is stored bottom-up as B, G, R triples. Weighted luminance
+           (ITU-R BT.601) written back to all three channels. */
+        for (int i = 0; i + 2 < imageSize; i += channels) {
+            unsigned char b = buffer[i];
+            unsigned char g = buffer[i + 1];
+            unsigned char r = buffer[i + 2];
+
+            unsigned char gray = (unsigned char)(
+                0.299 * r + 0.587 * g + 0.114 * b
+            );
+
+            buffer[i]     = gray;
+            buffer[i + 1] = gray;
+            buffer[i + 2] = gray;
+        }
     }
+    /* 8bpp (or lower) images are already single channel: nothing to do. */
 
     imageWriter(outputPath, header, colorTable,
                 buffer, width, height, bitDepth);
